98dcfba611eb88c0861ee40ee6b636b20b852f57
Jacob Jose <jjose14@asu.edu>
2023-05-28 16:05:48 -0700

    20230528_1419 12% Added 'edition Type' and 'year' to JavaScript matrix.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,13 @@
 
 const INITIAL_VALUE_MATRIX = 0;
 const ID_DIV_MATRIX = "divMatrix";
+const MATRIX_COLUMN_INDICES = {
+    COLUMN_INDEX_OF_EDITION_TYPE: 0,
+    COLUMN_INDEX_OF_YEAR: 1
+};
 
 // Global Variable declarations.
-let editionsType, yearStarting, yearEnding, volumeStartingYear, editionsPerYear;
+let editionsType, yearStarting, yearEnding, volumeYearStarting, editionsPerYear;
 let matrix, div_matrix;
 
 // Function to get input parameters. Created by ChatGPT.
@@ -13,10 +17,10 @@ document.getElementById("btnSubmit").addEventListener("click", function() {
     event.preventDefault();
 
     editionsType = document.getElementById("txtTextEditionsType").value;
-    yearStarting = document.getElementById("txtNumberYearStarting").value;
-    yearEnding = document.getElementById("txtNumberYearEnding").value;
-    volumeStartingYear = document.getElementById("txtNumberVolumeStartingYear").value;
-    editionsPerYear = document.getElementById("txtNumberEditionsPerYear").value;
+    yearStarting = parseInt(document.getElementById("txtNumberYearStarting").value);
+    yearEnding = parseInt(document.getElementById("txtNumberYearEnding").value);
+    volumeYearStarting = parseInt(document.getElementById("txtNumberVolumeStartingYear").value);
+    editionsPerYear = parseInt(document.getElementById("txtNumberEditionsPerYear").value);
     div_matrix = document.querySelector("#"+ID_DIV_MATRIX);
 
 
@@ -108,34 +112,32 @@ function displayMatrixAsTable(matrix) {
 
         const labelEditionType = document.createElement('label');
         labelEditionType.id = 'labelEditionType' + startingYear;
-        labelEditionType.innerHTML = editionsType + ". " + volumeStartingYear;
+        labelEditionType.innerHTML = matrix[i][MATRIX_COLUMN_INDICES.COLUMN_INDEX_OF_EDITION_TYPE];
 
-        volumeStartingYear++;
+        volumeYearStarting++;
 
         // Create text-fields for each year.
         const textFieldYear = document.createElement('input');
         textFieldYear.type = 'text'
-        textFieldYear.value = `${startingYear}`;
+        textFieldYear.value = matrix[i][MATRIX_COLUMN_INDICES.COLUMN_INDEX_OF_YEAR];
         textFieldYear.id = 'txtNumberYear' + startingYear;
 
 
-        // Create checkbox element
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.addEventListener('change', function() {
+        // Create a main checkbox for each year (year-checkbox).
+        const checkboxOfYear = document.createElement('input');
+        checkboxOfYear.type = 'checkbox';
+        checkboxOfYear.addEventListener('change', function() {
             const checkboxes = row.querySelectorAll('input[type="checkbox"]');
             for (let j = 0; j < checkboxes.length; j++) {
-                checkboxes[j].checked = checkbox.checked;
+                checkboxes[j].checked = checkboxOfYear.checked;
             }
         });
 
-        // Set checkbox value based on matrix item
-        checkbox.checked = false;
+        // Set year-checkbox value based on matrix item
+        checkboxOfYear.checked = false;
 
-
-
-        // Attach change event listener to checkbox
-        checkbox.addEventListener('change', function () {
+        // Attach change event listener to year-checkbox.
+        checkboxOfYear.addEventListener('change', function () {
             if (this.checked) {
                 // matrix[i][j] = 1; // Set matrix value to 1 if checkbox is checked
             } else {
@@ -144,16 +146,17 @@ function displayMatrixAsTable(matrix) {
             printToConsole(matrix);
         });
 
-        // Append checkbox to cell
+        // Append year information to row.
         row.appendChild(labelEditionType);
-        row.appendChild(checkbox);
+        row.appendChild(checkboxOfYear);
         row.appendChild(textFieldYear);
 
         row.appendChild(rowIndexCell);
         printToConsole("hello");
         startingYear++;
 
-        for (let j = 0; j < matrix[i].length; j++) {
+        // Creating individual checkbox for each edition of year.
+        for (let indexOfMatrixColumn = Object.keys(MATRIX_COLUMN_INDICES).length, indexOfEdition = 1; indexOfMatrixColumn < matrix[i].length; indexOfMatrixColumn++, indexOfEdition++) {
             const cell = document.createElement('td');
 
             // Create checkbox element
@@ -161,11 +164,11 @@ function displayMatrixAsTable(matrix) {
             checkbox.type = 'checkbox';
 
             // Set checkbox value based on matrix item
-            checkbox.checked = matrix[i][j] !== 0;
+            checkbox.checked = matrix[i][indexOfMatrixColumn] !== 0;
 
             // Create label for checkbox
             const label = document.createElement('label');
-            label.textContent = `${j+1}`;
+            label.textContent = `${indexOfEdition}`;
 
             // Attach change event listener to checkbox
             checkbox.addEventListener('change', function () {
@@ -193,6 +196,16 @@ function displayMatrixAsTable(matrix) {
     div_matrix.appendChild(table);
 }
 
+// Function will add edition type (i.e. 'vol. 1') and year (i.e. '2000') to specific column indices.
+function matrixAddEditionType(matrix) {
+
+    for(let i=0; i<matrix.length; i++) {
+        matrix[i][MATRIX_COLUMN_INDICES.COLUMN_INDEX_OF_EDITION_TYPE] = editionsType + ". " + (volumeYearStarting+ i);
+        matrix[i][MATRIX_COLUMN_INDICES.COLUMN_INDEX_OF_YEAR] = yearStarting + i;
+    }
+
+}
+
 // Function with main logic.
 function main() {
 
@@ -201,7 +214,9 @@ function main() {
 
     // Adding '+1' to ensure 'ending' year is also included.
     let numberOfYears = yearEnding - yearStarting + 1;
-    let matrixYearAndEditions = createMatrix(numberOfYears, editionsPerYear, INITIAL_VALUE_MATRIX);
+    // Adding '+2' to number of years to add 'edition number' and 'year' information.
+    let matrixYearAndEditions = createMatrix(numberOfYears, editionsPerYear + Object.keys(MATRIX_COLUMN_INDICES).length, INITIAL_VALUE_MATRIX);
+    matrixAddEditionType(matrixYearAndEditions);
 
     console.log("Matrix is :", JSON.stringify(matrixYearAndEditions));
 
