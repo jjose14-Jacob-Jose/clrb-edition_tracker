6a9682375ef0a720790fcf38d6257d8eb6b36488
Jacob Jose <jjose14@asu.edu>
2023-06-07 14:46:39 -0700

    20230607_1446 36% Added jUnit method for summarizing method. Made bug fixes.

--- src/main/java/org/clrb/editiontracker/service/EditionService.java
+++ src/main/java/org/clrb/editiontracker/service/EditionService.java
@@ -1,53 +1,44 @@
 package org.clrb.editiontracker.service;
 
 import org.clrb.editiontracker.constants.EditionConstants;
-import org.clrb.editiontracker.model.YearEdition;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EditionService {
 
+//    Get a range of indexes that have the 'identifyingValue'.
+    public List<String> getArraySummaryWithStartIndexOne(int [] arrayDetailed, int identifyingValue) {
 
+        List<String> listArraySummarized = new ArrayList<String>();
+        int indexOffset = 1;
 
-    public List<String> getArraySummary(int [] arrayDetailed, int identifyingValue) {
+        if (arrayDetailed.length == 1) {
+            if (arrayDetailed[0] == identifyingValue)
+                listArraySummarized.add(String.valueOf(indexOffset));
 
-        if (arrayDetailed.length < 1) {
-            return null;
-        } else if (arrayDetailed.length == 1) {
-            if(arrayDetailed[0] == identifyingValue) {
-                return new ArrayList<>(List.of("1"));
-            } else {
-                return null;
-            }
-        }
-        else {
-            List<String> listArraySummarized = new ArrayList<String>();
+        } else if (arrayDetailed.length >= 1) {
             int issueRangeStart, issueRangeEnd;
             issueRangeStart = issueRangeEnd = EditionConstants.INVALID_VALUE_INTEGER;
-            for(int i=0; i<arrayDetailed.length; i++) {
+            for (int i = 0; i < arrayDetailed.length; i++) {
                 if (arrayDetailed[i] == identifyingValue) {
                     if (issueRangeStart == EditionConstants.INVALID_VALUE_INTEGER)
                         issueRangeStart = issueRangeEnd = i;
-                    else if ( i == (issueRangeEnd + 1) )
+                    else if (i == (issueRangeEnd + 1))
                         issueRangeEnd = i;
-                } else {
+                }
+                if ((arrayDetailed[i] != identifyingValue) || (i == arrayDetailed.length - 1)) {
                     if (issueRangeStart != EditionConstants.INVALID_VALUE_INTEGER) {
                         if (issueRangeStart == issueRangeEnd)
-                            listArraySummarized.add(String.valueOf(issueRangeStart));
-                         else
-                            listArraySummarized.add(String.valueOf(issueRangeStart) + EditionConstants.SEPARATOR_ISSUES + String.valueOf(issueRangeEnd));
+                            listArraySummarized.add(String.valueOf(issueRangeStart + indexOffset));
+                        else
+                            listArraySummarized.add(String.valueOf(issueRangeStart + indexOffset) + EditionConstants.SEPARATOR_ISSUES + String.valueOf(issueRangeEnd + indexOffset));
                     }
                     issueRangeStart = issueRangeEnd = EditionConstants.INVALID_VALUE_INTEGER;
                 }
-
             }
-            return listArraySummarized;
-
         }
-
-
+        return listArraySummarized;
     }
-
 
 }
--- src/test/java/org/clrb/editiontracker/service/EditionServiceTest.java
+++ src/test/java/org/clrb/editiontracker/service/EditionServiceTest.java
@@ -3,21 +3,68 @@ package org.clrb.editiontracker.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class EditionServiceTest {
 
     @Test
-    void getArraySummary() {
+    void getArraySummaryOne() {
         EditionService editionService = new EditionService();
 
         int [] inputArrayDetailed = {1, 1, 0, 0, 1, 1, 1, 0};
         int inputIdentifyingValue = 1;
-        List<String> listActual = List.of("0-1", "4-6");
+        List<String> listActual = List.of("1-2", "5-7");
 
-        List<String> listExpected = editionService.getArraySummary(inputArrayDetailed, inputIdentifyingValue);
-
+        List<String> listExpected = editionService.getArraySummaryWithStartIndexOne(inputArrayDetailed, inputIdentifyingValue);
         Assertions.assertArrayEquals(listExpected.toArray(), listActual.toArray());
+    }
 
+    @Test
+    void getArraySummaryTwo() {
+        EditionService editionService = new EditionService();
+
+        int [] inputArrayDetailed = {1, 1, 0, 0, 1, 1, 1, 1};
+        int inputIdentifyingValue = 1;
+        List<String> listActual = List.of("1-2", "5-8");
+
+        List<String> listExpected = editionService.getArraySummaryWithStartIndexOne(inputArrayDetailed, inputIdentifyingValue);
+        Assertions.assertArrayEquals(listExpected.toArray(), listActual.toArray());
+    }
+
+    @Test
+    void getArraySummaryThree() {
+        EditionService editionService = new EditionService();
+
+        int [] inputArrayDetailed = {1, 1, 0, 1, 0, 1, 1, 0, 1};
+        int inputIdentifyingValue = 1;
+        List<String> listActual = List.of("1-2", "4", "6-7", "9");
+
+        List<String> listExpected = editionService.getArraySummaryWithStartIndexOne(inputArrayDetailed, inputIdentifyingValue);
+        Assertions.assertArrayEquals(listExpected.toArray(), listActual.toArray());
+    }
+
+    @Test
+    void getArraySummaryFour() {
+        EditionService editionService = new EditionService();
+
+        int [] inputArrayDetailed = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int inputIdentifyingValue = 1;
+        List<String> listActual = new ArrayList<>(0);
+
+        List<String> listExpected = editionService.getArraySummaryWithStartIndexOne(inputArrayDetailed, inputIdentifyingValue);
+        Assertions.assertArrayEquals(listExpected.toArray(), listActual.toArray());
+    }
+
+    @Test
+    void getArraySummaryFive() {
+        EditionService editionService = new EditionService();
+
+        int [] inputArrayDetailed = {1, 1, 1, 1, 1, 1, 1};
+        int inputIdentifyingValue = 1;
+        List<String> listActual = List.of("1-7");;
+
+        List<String> listExpected = editionService.getArraySummaryWithStartIndexOne(inputArrayDetailed, inputIdentifyingValue);
+        Assertions.assertArrayEquals(listExpected.toArray(), listActual.toArray());
     }
 }
