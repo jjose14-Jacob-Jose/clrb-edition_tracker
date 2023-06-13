1d346cca9ef10438780dbb6e094ed0677dc106c4
Jacob Jose <jjose14@asu.edu>
2023-06-13 16:01:07 -0700

    20230613_1600 50% Able to render output from Java API to HTML front-end.

--- src/main/java/org/clrb/editiontracker/controller/EditionController.java
+++ src/main/java/org/clrb/editiontracker/controller/EditionController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -25,12 +26,15 @@ public class EditionController {
     @GetMapping("/")
     public String home() {
         System.out.println("EditionController.java: @GetMapping(\"/\")");
-        return "index.html";
+        return "index";
     }
 
     @PostMapping("/postData")
     public String processData(@ModelAttribute HTMLFormInformation htmlFormInformation, Model model) {
         editionService.getModel(htmlFormInformation, model, "index");
-        return "redirect:/index.html";
+        System.out.println("model: "+model.toString());
+        return "index";
+
     }
+
 }
